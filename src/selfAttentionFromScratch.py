7b6a7c075ecfe7418a2eafdcd365da9aee56d891
mkeller@arch <michaelkeller1997@gmail.com>
2025-01-21 18:13:25 -0500

    embedded sentence

--- src/selfAttentionFromScratch.py
+++ src/selfAttentionFromScratch.py
@@ -1,3 +1,4 @@
+# Embedding an input sentence 
 sentence = 'Lift is short, eat dessert first'
 
 dc = {s:i for i,s in enumerate(sorted(sentence.replace(',', '').split()))}
@@ -7,3 +8,12 @@ import torch
 
 sentence_int = torch.tensor([dc[s] for s in sentence.replace(',','').split()])
 print(sentence_int)
+
+torch.manual_seed(123)
+embed = torch.nn.Embedding(6, 16)
+embedded_sentence = embed(sentence_int).detach()
+
+print(embedded_sentence)
+print(embedded_sentence.shape)
+
+# Defining the weight matrices 
